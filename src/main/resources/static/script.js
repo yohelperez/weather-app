37224fcbd8e2980e47b96a7c506238c876726a7e
yohelperez <perezyohel@gmail.com>
2024-10-14 21:50:30 -0500

    Change weather info on frontend

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -11,10 +11,10 @@ async function getWeather() {
     document.getElementById('result').innerHTML = `
         <div class="bg-white p-4 shadow-md rounded-lg inline-block max-w-xs">
             <img src="${data.iconUrl}" alt="Weather Icon" class="mx-auto mb-4 w-20 h-20">
-            <p class="text-xl font-semibold mb-2 text-blue-600"><b>Weather:</b> ${data.weather}</p>
-            <p class="text-gray-700"><b>Details:</b> ${data.details}</p>
-            <p class="text-gray-700"><b>Temperature:</b> ${data.temperature}°C</p>
+            <p class="text-xl font-semibold mb-2 text-blue-600"><b>Temperature:</b> ${data.temperature}°C</p>
             <p class="text-gray-700"><b>Real Feel:</b> ${data.realFeel}°C</p>
+            <p class="text-gray-700"><b>Weather:</b> ${data.weather}</p>
+            <p class="text-gray-700"><b>Details:</b> ${data.details}</p>
         </div>
     `;
 }
